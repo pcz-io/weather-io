d2861aa0b9c09e50cbad8e6636a6cbb6822cf7f3
Damblor <cieplutkaKasia@protonmail.com>
2023-11-12 16:14:18 +0100

    Removed unneeded files

--- WeatherIO/WeatherIO.Common/Data/Interfaces/IWeatherForecastService.cs
+++
@@ -1,9 +0,0 @@
-﻿using WeatherIO.Common.Data.Models;
-
-namespace WeatherIO.Common.Data.Interfaces
-{
-    public interface IWeatherForecastService
-    {
-        Task<WeatherForecast[]> GetForecastAsync(DateTime startDate);
-    }
-}
