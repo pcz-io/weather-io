fa9183ee900ba2a1a2f7753ad8a0b3b22d694634
siatkabiedy <kamilopa6@gmail.com>
2023-12-05 18:44:21 +0100

    Completes common project documentation

--- WeatherIO/WeatherIO.Common/Data/Models/APIResponses/GeocodeResponse.cs
+++ WeatherIO/WeatherIO.Common/Data/Models/APIResponses/GeocodeResponse.cs
@@ -1,10 +1,28 @@
 ﻿namespace WeatherIO.Common.Data.Models.APIResponses
 {
+    /// <summary>
+    /// Represents response data from `geocode` and `geocode-by-id` endpoints
+    /// </summary>
     public class GeocodeResponse
     {
+        /// <summary>
+        /// Id of the city
+        /// </summary>
         public int Id { get; set; }
+
+        /// <summary>
+        /// Name of the city
+        /// </summary>
         public string CityName { get; set; }
+
+        /// <summary>
+        /// Longitude of the city
+        /// </summary>
         public double Longitude { get; set; }
+
+        /// <summary>
+        /// Latitude of the city
+        /// </summary>
         public double Latitude { get; set; }
     }
 }
--- WeatherIO/WeatherIO.Common/Data/Models/RegisterModel.cs
+++ WeatherIO/WeatherIO.Common/Data/Models/RegisterModel.cs
@@ -2,12 +2,21 @@
 
 namespace WeatherIO.Common.Data.Models
 {
+    /// <summary>
+    /// Represents POST data sent to the `register` endpoint
+    /// </summary>
     public class RegisterModel
     {
+        /// <summary>
+        /// Email address of the user
+        /// </summary>
         [EmailAddress]
         [Required(ErrorMessage = "Email is required")]
         public string Email { get; set; }
 
+        /// <summary>
+        /// Users password
+        /// </summary>
         [Required(ErrorMessage = "Password is required")]
         public string Password { get; set; }
     }
