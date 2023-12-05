784d39bea24c8bd732c3b19f37af7afe61a98cdd
Damblor <cieplutkaKasia@protonmail.com>
2023-12-05 19:20:31 +0100

    Added favourites support (#22)
    - Added FavouriteProviderService
    - Modified HttpClientProviderService
    - Modified NavMenu and Weather pages to support variable passing

---
+++ WeatherIO/WeatherIO.Common/Data/Interfaces/IFovouriteProviderService.cs
@@ -0,0 +1,11 @@
+﻿using WeatherIO.Common.Data.Models;
+
+namespace WeatherIO.Common.Data.Interfaces
+{
+    public interface IFovouriteProviderService
+    {
+        Task<bool> DeleteFavouriteAsync(FavouriteLocationModel model);
+        Task<List<FavouriteLocationModel>?> GetFavouritesAsync();
+        Task<bool> SetOrUpdateFavouriteAsync(FavouriteLocationModel model);
+    }
+}
